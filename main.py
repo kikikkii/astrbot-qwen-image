9e4db32f9afc14295e37d1a1a85a6e1129d625bb
lbh <948677135@qq.com>
2026-06-30 08:11:19 +0800

    修复导入错误

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult, AstrbotConfig
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+from astrbot.api import logger, AstrbotConfig
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
